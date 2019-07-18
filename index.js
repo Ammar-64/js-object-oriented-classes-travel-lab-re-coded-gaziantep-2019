d6b9a5764c1bb0733091614d5344445953794fa7
Ammar Sammour <ammar.n.sammour@gmail.com>
2019-07-18 12:56:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ class Driver{
     this.startDate = new Date(startDate);
   }
   yearsExperienceFromBeginningOf(year){
-    return new Date().getFullYear() - this.startDate.getFullYear();
+    return year - this.startDate.getFullYear();
   }
 }
